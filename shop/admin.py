105017b761ceb6cc85ba3213cf150516f3913276
Савчук Андрей <a.savchuk@netrika.ru>
2024-11-16 20:44:44 +0700

    Настроена админка

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from . import models
 
+admin.site.site_header = "Заголовок"
+admin.site.site_title = "Uslugi"
+admin.site.index_title = "Описание услуг"
+
+
 admin.site.register(models.Category)
 admin.site.register(models.Course)
 
