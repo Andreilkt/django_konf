f888999b5a99c0ab07718b8aaf020beebbe095d8
Савчук Андрей <a.savchuk@netrika.ru>
2024-11-16 21:59:54 +0700

    изменена  админка

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
-from . import models
+from .models import Course, Category
 
 admin.site.site_header = "Заголовок"
-admin.site.site_title = "Uslugi"
+admin.site.site_title = "Парапланерный сервис"
 admin.site.index_title = "Описание услуг"
 
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+
+class CoursesInline(admin.TabularInline):
+    model = Course
+    exclude = ['created_at']
+    extra = 1
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        # ('Dates', {
+        #     'fields': ['title'],
+        #     'classes': ['collapse']
+        # }
+        #  )
+
+    ]
+
+    inlines = [CoursesInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course, CourseAdmin)
 
